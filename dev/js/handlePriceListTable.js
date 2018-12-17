dd005979b2b45b3429aab932e5b322dfe8196a29
hopes91 <nadya.me91@gmail.com>
2018-12-17 11:47:28 +0300

    add scrolling to the price list table and remove padding when <= 640px

---
+++ dev/js/handlePriceListTable.js
@@ -0,0 +1,11 @@
+const main = document.querySelector('main');
+
+const handlePriceListTable = () => {
+  if (main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
+    main.style.padding = '1em 0';
+  } else {
+    main.style.padding = '1em';
+  }
+}
+
+window.addEventListener('resize', handlePriceListTable);
