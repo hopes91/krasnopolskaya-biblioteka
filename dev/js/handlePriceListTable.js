ead0616fa00a75183b5010fa1ba5467db48faa15
hopes91 <nadya.me91@gmail.com>
2018-12-17 22:25:52 +0300

    fix else if

--- dev/js/handlePriceListTable.js
+++ dev/js/handlePriceListTable.js
@@ -3,8 +3,10 @@ const main = document.querySelector('main');
 const handlePriceListTable = () => {
   if (main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
     main.style.padding = '1em 0';
-  } else {
+  } else if (!main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
     main.style.padding = '1em 0.5em';
+  } else {
+    main.style.padding = '1em';
   }
 }
 
