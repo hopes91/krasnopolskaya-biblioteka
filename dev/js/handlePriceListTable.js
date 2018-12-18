a97afac7981ecbde1969cc59bef20b01824cd7af
hopes91 <nadya.me91@gmail.com>
2018-12-18 21:37:09 +0300

    hide ornament on the price list page when the width <= 640

--- dev/js/handlePriceListTable.js
+++ dev/js/handlePriceListTable.js
@@ -1,12 +1,15 @@
 const main = document.querySelector('main');
+const ornaments = document.querySelectorAll('.ornament');
 
 const handlePriceListTable = () => {
   if (main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
     main.style.padding = '1em 0';
+    ornaments.forEach(ornament => ornament.style.display = 'none');
   } else if (!main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
     main.style.padding = '1em 0.5em';
   } else {
     main.style.padding = '1em';
+    ornaments.forEach(ornament => ornament.style.display = 'inline-block');
   }
 }
 
