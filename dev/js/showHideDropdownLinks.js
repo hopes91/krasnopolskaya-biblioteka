dd59e2498949abd850004fff4c9aac08cf783286
hopes91 <nadya.me91@gmail.com>
2018-12-18 08:48:24 +0300

    do the same after clicking a span

--- dev/js/showHideDropdownLinks.js
+++ dev/js/showHideDropdownLinks.js
@@ -1,8 +1,19 @@
 const dropdownSections = document.querySelectorAll('.dropdown-section');
 
 const toggleDropdownLinks = event => {
+  let parent = event.target.parentNode;
+  
   if (event.target.className === 'section-title') {
     let dropdownLinks = event.target.nextElementSibling;
+
+    if (dropdownLinks.style.display === 'block') {
+      dropdownLinks.style.display = 'none';
+    } else {
+      dropdownLinks.style.display = 'block';
+    }
+  } else if (parent.className === 'section-title') {
+    let dropdownLinks = event.target.parentNode.nextElementSibling;
+
     if (dropdownLinks.style.display === 'block') {
       dropdownLinks.style.display = 'none';
     } else {
