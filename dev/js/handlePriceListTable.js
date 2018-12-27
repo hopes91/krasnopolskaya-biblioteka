e0aaf11b7699737a6191232c1bfcbd7ac20974d1
hopes91 <nadya.me91@gmail.com>
2018-12-27 14:25:47 +0300

    fix: hide ornament on tiny mobile screens

--- dev/js/handlePriceListTable.js
+++ dev/js/handlePriceListTable.js
@@ -2,12 +2,12 @@ const main = document.querySelector('main');
 const ornaments = document.querySelectorAll('.ornament');
 
 const handlePriceListTable = () => {
-  if (main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
+  if (main.children[1].className.includes('price-list') && window.innerWidth <= 640) {
     main.style.padding = '1em 0';
     ornaments.forEach(ornament => ornament.style.display = 'none');
-  } else if (!main.children[0].className.includes('price-list') && window.innerWidth <= 640) {
+  } else if (!main.children[1].className.includes('price-list') && window.innerWidth <= 640) {
     main.style.padding = '1em 0.5em';
-  } else {
+  } else if (window.innerWidth > 640) {
     main.style.padding = '1em';
     ornaments.forEach(ornament => ornament.style.display = 'inline-block');
   }
