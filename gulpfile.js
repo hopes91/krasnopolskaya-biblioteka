c0eaac0c012462a7b8cf09c8cd46214697229f96
Nadya F-a <hopes91@users.noreply.github.com>
2019-01-10 14:31:59 +0300

    remove gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const uglifycss = require('gulp-uglifycss');
 const babel = require('gulp-babel');
-const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cached = require('gulp-cached');
 const remember = require('gulp-remember');
