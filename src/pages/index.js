36cfc52fb73e0bd39ee288df07e2ae9e0ae673e7
hopes91 <nadya.me91@gmail.com>
2019-04-03 11:11:27 +0300

    add main content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import OrnamentMain from '../components/ornamentMain'
+import newsOne from '../images/news/24032019-2.jpg'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Главная' keywords={[`краснопольская библиотека`, `краснополье`, `сунский район`, `суна`, `кировская область`]} />
+    <div className='news'>
+      <OrnamentMain />
+      <h2>Новости</h2>
+      <section>
+        <h3>24 марта 2019 год</h3>
+        <img src={newsOne} alt='Благодарность Пермяковой Л. А.' />
+      </section>
+    </div>
   </Layout>
 )
 
