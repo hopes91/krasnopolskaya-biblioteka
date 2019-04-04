903ecd64dc79bd53644b1f49ef91d16af81b57c5
hopes91 <nadya.me91@gmail.com>
2019-04-04 17:34:06 +0300

    add content to NotFoundPage

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,16 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import OrnamentMain from '../components/ornamentMain'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title='404: Страница не найдена' />
-    <h1>СТРАНИЦА НЕ НАЙДЕНА</h1>
-    <p>Вы попали на страницу, которой не существует. Перейти на <Link to='/'>главную</Link>.</p>
+    <SEO title='404: Страница не найдена' keywords={[`краснопольская библиотека`, `краснополье`, `сунский район`, `суна`, `кировская область`]} />
+    <div className='not-found-page'>
+      <OrnamentMain />
+      <h2>СТРАНИЦА НЕ НАЙДЕНА</h2>
+      <p>Вы попали на страницу, которой не существует. Перейти на <Link to='/'>главную</Link>.</p>
+    </div>
   </Layout>
 )
 
