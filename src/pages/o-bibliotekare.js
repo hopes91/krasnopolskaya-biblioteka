baff38c937a391a09105ee3516e484b2c4e6b70e
hopes91 <nadya.me91@gmail.com>
2019-04-04 19:06:24 +0300

    add content to AboutLibrarianPage

--- src/pages/o-bibliotekare.js
+++ src/pages/o-bibliotekare.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import OrnamentMain from '../components/ornamentMain'
 
 const AboutTheLibrarianPage = () => (
   <Layout>
     <SEO title='О библиотекаре' keywords={[`о библиотекаре`, `фалалеева елена евгеньевна`, `краснопольская библиотека`, `краснополье`, `сунский район`, `суна`, `кировская область`]} />
+    <div className='about-library about-librarian'>
+      <OrnamentMain />
+      <h2>О библиотекаре</h2>
+      <p>Со 2 августа 1982 года библиотекарем в Краснопольской библиотеке работает Елена Евгеньевна Опарина (Фалалеева). Елена Евгеньевна имеет высшее библиотечное образование, в 1988 закончила Пермский институт культуры. В настоящее время работает на полставки.</p>
+    </div>
   </Layout>
 )
 
