167001547d0b6505771d5f1622df4b9d98b53467
hopes91 <nadya.me91@gmail.com>
2019-04-05 11:27:27 +0300

    refactor photos lists

--- src/pages/fotogalereya.js
+++ src/pages/fotogalereya.js
@@ -4,7 +4,7 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import OrnamentMain from '../components/ornamentMain'
 
-import semejnyjKlub29Marta1 from '../images/photogalery/semejnyj-klub-29-marta.jpg'
+import semejnyjKlub29Marta from '../images/photogalery/semejnyj-klub-29-marta.jpg'
 import semejnyjKlub29Marta2 from '../images/photogalery/semejnyj-klub-29-marta-2.jpg'
 import biblionik from '../images/photogalery/biblionik.jpg'
 import chtoGdeKogdaPoPravu from '../images/photogalery/chto-gde-kogda-po-pravu.jpg'
@@ -22,6 +22,31 @@ import otkryvaemYponiju from '../images/photogalery/otkryvaem-yaponiju.jpg'
 import novogodnijVecherNaSemejnomKlube from '../images/photogalery/novogodnij-vecher-na-semejnom-klube.jpg'
 import novogodnijVecherNaSemejnomKlube2 from '../images/photogalery/novogodnij-vecher-na-semejnom-klube-2.jpg'
 import novogodnijVecherNaSemejnomKlube3 from '../images/photogalery/novogodnij-vecher-na-semejnom-klube-3.jpg'
+
+const photos2019 = [
+  { src: semejnyjKlub29Marta, alt: 'Семейный клуб 29 марта' },
+  { src: semejnyjKlub29Marta2, alt: 'Семейный клуб 29 марта 2' }
+]
+const photos2018 = [
+  { src: biblionik, alt: 'Библионик' },
+  { src: chtoGdeKogdaPoPravu, alt: 'Что, где, когда по праву' },
+  { src: chtoGdeKogdaPoPravu2, alt: 'Что, где, когда по праву 2' },
+  { src: kvestVoVremyaNochiPottera, alt: 'Квест во время ночи Поттера' },
+  { src: garriPotter, alt: 'Гарри Поттер' },
+  { src: vedma, alt: 'Ведьма' },
+  { src: horoshoSidim, alt: 'Хороши сидим' },
+  { src: katyaSupermama, alt: 'Катя — супермама' },
+  { src: luchshieNaryady, alt: 'Лучшие наряды' },
+  { src: lukomorieK45letijuBiblioteki, alt: 'Лукоморье к 45-летию библиотеки' },
+  { src: nochIskusstv, alt: 'Ночь искусств' },
+  { src: tort, alt: 'Торт' },
+  { src: otkryvaemYponiju, alt: 'Открываем Японию' },
+  { src: novogodnijVecherNaSemejnomKlube, alt: 'Новогодний вечер на семейном клубе' },
+  { src: novogodnijVecherNaSemejnomKlube2, alt: 'Новогодний вечер на семейном клубе 2' },
+  { src: novogodnijVecherNaSemejnomKlube3, alt: 'Новогодний вечер на семейном клубе 3' }
+]
+
+const allPhotos = photos2019.concat(photos2018)
 
 const miniPhotos = document.querySelectorAll('.mini-photo')
 const back = document.getElementById('back')
@@ -106,50 +131,46 @@ const PhotogalleryPage = () => (
       <h2>Фотогалерея</h2>
       <h3>2019 год</h3>
       <div className='sorted-by-year'>
-        <img src={semejnyjKlub29Marta1} alt='Семейный клуб 29 марта' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={semejnyjKlub29Marta2} alt='Семейный клуб 29 марта 2' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
+        {photos2019.map(photo => {
+          const { src, alt } = photo
+
+          return (
+            <img
+              src={src} alt={alt} title='Нажмите, чтобы посмотреть в большом разрешении' key={src}
+              className='mini-photo'
+              onClick={showFirstSlide}
+            />
+          )
+        })}
       </div>
       <h3>2018 год</h3>
       <div className='sorted-by-year'>
-        <img src={biblionik} alt='Библионик' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={chtoGdeKogdaPoPravu} alt='Что, где, когда по праву' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={chtoGdeKogdaPoPravu2} alt='Что, где, когда по праву 2' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={kvestVoVremyaNochiPottera} alt='Квест во время ночи Поттера' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={garriPotter} alt='Гарри Поттер' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={vedma} alt='Ведьма' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={horoshoSidim} alt='Хороши сидим' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={katyaSupermama} alt='Катя — супермама' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={luchshieNaryady} alt='Лучшие наряды' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={lukomorieK45letijuBiblioteki} alt='Лукоморье к 45-летию библиотеки' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={nochIskusstv} alt='Ночь искусств' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={tort} alt='Торт' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={otkryvaemYponiju} alt='Открываем Японию' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={novogodnijVecherNaSemejnomKlube} alt='Новогодний вечер на семейном клубе' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={novogodnijVecherNaSemejnomKlube2} alt='Новогодний вечер на семейном клубе 2' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
-        <img src={novogodnijVecherNaSemejnomKlube3} alt='Новогодний вечер на семейном клубе 3' title='Нажмите, чтобы посмотреть в большом разрешении' className='mini-photo' onClick={showFirstSlide} />
+        {photos2018.map(photo => {
+          const { src, alt } = photo
+
+          return (
+            <img
+              src={src} alt={alt} title='Нажмите, чтобы посмотреть в большом разрешении' key={src}
+              className='mini-photo'
+              onClick={showFirstSlide}
+            />
+          )
+        })}
       </div>
     </div>
     <div id='back'>
-      <span id='close' title='Закрыть' onClick={hideSlider}>&times;</span>
+      <span title='Закрыть' id='close' onClick={hideSlider}>&times;</span>
       <div id='front'>
-        <img className='slide' src={semejnyjKlub29Marta1} alt='Семейный клуб 29 марта' title='Семейный клуб 29 марта' />
-        <img className='slide' src={semejnyjKlub29Marta2} alt='Семейный клуб 29 марта 2' title='Семейный клуб 29 марта 2' />
-        <img className='slide' src={biblionik} alt='Библионик' title='Библионик' />
-        <img className='slide' src={chtoGdeKogdaPoPravu} alt='Что, где, когда по праву' title='Что, где, когда по праву' />
-        <img className='slide' src={chtoGdeKogdaPoPravu2} alt='Что, где, когда по праву 2' title='Что, где, когда по праву 2' />
-        <img className='slide' src={kvestVoVremyaNochiPottera} alt='Квест во время ночи Поттера' title='Квест во время ночи Поттера' />
-        <img className='slide' src={garriPotter} alt='Гарри Поттер' title='Гарри Поттер' />
-        <img className='slide' src={vedma} alt='Ведьма' title='Ведьма' />
-        <img className='slide' src={horoshoSidim} alt='Хороши сидим' title='Хороши сидим' />
-        <img className='slide' src={katyaSupermama} alt='Катя — супермама' title='Катя — супермама' />
-        <img className='slide' src={luchshieNaryady} alt='Лучшие наряды' title='Лучшие наряды' />
-        <img className='slide' src={lukomorieK45letijuBiblioteki} alt='Лукоморье к 45-летию библиотеки' title='Лукоморье к 45-летию библиотеки' />
-        <img className='slide' src={nochIskusstv} alt='Ночь искусств' title='Ночь искусств' />
-        <img className='slide' src={tort} alt='Торт' title='Торт' />
-        <img className='slide' src={otkryvaemYponiju} alt='Открываем Японию' title='Открываем Японию' />
-        <img className='slide' src={novogodnijVecherNaSemejnomKlube} alt='Новогодний вечер на семейном клубе' title='Новогодний вечер на семейном клубе' />
-        <img className='slide' src={novogodnijVecherNaSemejnomKlube2} alt='Новогодний вечер на семейном клубе 2' title='Новогодний вечер на семейном клубе 2' />
-        <img className='slide' src={novogodnijVecherNaSemejnomKlube3} alt='Новогодний вечер на семейном клубе 3' title='Новогодний вечер на семейном клубе 3' />
+        {allPhotos.map(photo => {
+          const { src, alt } = photo
+
+          return (
+            <img
+              src={src} alt={alt} title={alt} key={src}
+              className='slide'
+            />
+          )
+        })}
       </div>
       <span className='prev-slide' onClick={() => moveSlides(-1)}>&#10094;</span>
       <span className='next-slide' onClick={() => moveSlides(1)}>&#10095;</span>
