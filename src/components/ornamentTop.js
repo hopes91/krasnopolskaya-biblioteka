0dfde854a44943b38440e59c05c2a3f484f33729
hopes91 <nadya.me91@gmail.com>
2019-04-07 15:10:32 +0300

    fix displaying today's day of the week

--- src/components/ornamentTop.js
+++ src/components/ornamentTop.js
@@ -24,13 +24,13 @@ class OrnamentTop extends Component {
     let getYear = today.getFullYear()
 
     const daysOfTheWeek = {
+      0: 'Воскресенье',
       1: 'Понедельник',
       2: 'Вторник',
       3: 'Среда',
       4: 'Четверг',
       5: 'Пятница',
-      6: 'Суббота',
-      7: 'Воскресенье'
+      6: 'Суббота'
     }
 
     for (let day in daysOfTheWeek) {
