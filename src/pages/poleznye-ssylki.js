cd866118355a888d4bf769c7be40d405e09994fe
hopes91 <nadya.me91@gmail.com>
2019-04-17 16:05:08 +0300

    refactor the useful links

--- src/pages/poleznye-ssylki.js
+++ src/pages/poleznye-ssylki.js
@@ -4,26 +4,36 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import OrnamentMain from '../components/ornamentMain'
 
+const usefulLinks = [
+  { href: 'http://sunalib.ru/', title: 'Сунская центральная библиотека имени Ф.Ф. Павленкова' },
+  { href: 'http://www.herzenlib.ru/', title: 'Кировская областная библиотека имени А.И. Герцена' },
+  { href: 'https://bibliotekagrina.ru/', title: 'Кировская областная библиотека для детей и юношества имени А.С. Грина' },
+  { href: 'http://lihanovlib.ru/', title: 'Библиотека для детей и юношества имени Альберта Лиханова' },
+  { href: 'http://childbook.ru/o-biblioteke/', title: 'Липецкая областная детская библиотека' },
+  { href: 'https://bibliomaniya.blogspot.com/', title: 'Блог БИБЛИОМАНИЯ' },
+  { href: 'http://kum-biblio.ru/', title: 'Библиотека имени А.В. Фищева' },
+  { href: 'http://www.rgub.ru/', title: 'Российская государственная библиотека для молодёжи' },
+  { href: 'https://rgdb.ru/', title: 'Российская государственная детская библиотека' },
+  { href: 'http://arch.rgdb.ru/xmlui/', title: 'Национальная электронная детская библиотека' },
+  { href: 'https://xn--90ax2c.xn--p1ai/', title: 'Национальная электронная библиотека' },
+  { href: 'https://www.rsl.ru/', title: 'Российская государственная библиотека' },
+  { href: 'http://childlib.kmtn.ru/index.htm', title: 'Костромская областная детская библиотека имени Аркадия Гайдара' },
+  { href: 'http://www.pushkin-vyatka.ru/', title: 'Центральная городская библиотека имени А.С. Пушкина' }
+]
+
 const UsefulLinksPage = () => (
   <Layout>
     <SEO title='Полезные ссылки' keywords={[`полезные ссылки`, `краснопольская библиотека`, `краснополье`, `сунский район`, `суна`, `кировская область`]} />
     <div className='services useful-links'>
       <OrnamentMain />
       <h2>Полезные ссылки</h2>
-      <a href='http://sunalib.ru/' target='_blank' rel='noopener noreferrer'>Сунская центральная библиотека имени Ф. Ф. Павленкова</a>
-      <a href='http://www.herzenlib.ru/' target='_blank' rel='noopener noreferrer'>Кировская областная библиотека имени А. И. Герцена</a>
-      <a href='https://bibliotekagrina.ru/' target='_blank' rel='noopener noreferrer'>Кировская областная библиотека для детей и юношества имени А.С. Грина</a>
-      <a href='http://lihanovlib.ru/' target='_blank' rel='noopener noreferrer'>Библиотека для детей и юношества имени Альберта Лиханова</a>
-      <a href='http://childbook.ru/o-biblioteke/' target='_blank' rel='noopener noreferrer'>Липецкая областная детская библиотека</a>
-      <a href='https://bibliomaniya.blogspot.com/' target='_blank' rel='noopener noreferrer'>Блог БИБЛИОМАНИЯ</a>
-      <a href='http://kum-biblio.ru/' target='_blank' rel='noopener noreferrer'>Библиотека имени А. В. Фищева</a>
-      <a href='http://www.rgub.ru/' target='_blank' rel='noopener noreferrer'>Российская государственная библиотека для молодёжи</a>
-      <a href='https://rgdb.ru/' target='_blank' rel='noopener noreferrer'>Российская государственная детская библиотека</a>
-      <a href='http://arch.rgdb.ru/xmlui/' target='_blank' rel='noopener noreferrer'>Национальная электронная детская библиотека</a>
-      <a href='https://xn--90ax2c.xn--p1ai/' target='_blank' rel='noopener noreferrer'>Национальная электронная библиотека</a>
-      <a href='https://www.rsl.ru/' target='_blank' rel='noopener noreferrer'>Российская государственная библиотека</a>
-      <a href='http://childlib.kmtn.ru/index.htm' target='_blank' rel='noopener noreferrer'>Костромская областная детская библиотека имени Аркадия Гайдара</a>
-      <a href='http://www.pushkin-vyatka.ru/' target='_blank' rel='noopener noreferrer'>Центральная городская библиотека имени А. С. Пушкина</a>
+      {usefulLinks.map(link => {
+        const { href, title } = link
+
+        return (
+          <a href={href} target='_blank' rel='noopener noreferrer'>{title}</a>
+        )
+      })}
     </div>
   </Layout>
 )
