2d2dd0fcc83c003eae4483601e1515e4a73edb9f
hopes91 <nadya.me91@gmail.com>
2019-05-20 10:00:17 +0300

    refactor getTodaysDate

--- dev/js/getTodaysDate.js
+++ dev/js/getTodaysDate.js
@@ -14,47 +14,26 @@ const getTodaysDate = () => {
     let monthStr = null;
     let year = today.getFullYear();
 
-    let daysOfTheWeek = {
-      0: 'Воскресенье',
-      1: 'Понедельник',
-      2: 'Вторник',
-      3: 'Среда',
-      4: 'Четверг',
-      5: 'Пятница',
-      6: 'Суббота'
-    };
+    const daysOfTheWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
-    for (let dayOfTheWeek in daysOfTheWeek) {
-      if (dayOfTheWeekNum == dayOfTheWeek) {
-        dayOfTheWeekStr = daysOfTheWeek[dayOfTheWeek];
+    daysOfTheWeek.forEach((day, ind) => {
+      if (ind == dayOfTheWeekNum) {
+        dayOfTheWeekStr = day;
       }
-    }
+    });
 
-    let months = {
-      0: 'января',
-      1: 'февраля',
-      2: 'марта',
-      3: 'апреля',
-      4: 'мая',
-      5: 'июня',
-      6: 'июля',
-      7: 'августа',
-      8: 'сентября',
-      9: 'октября',
-      10: 'ноября',
-      11: 'декабря'
-    };
+    const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-    for (let month in months) {
-      if (monthNum == month) {
-        monthStr = months[month];
+    months.forEach((month, ind) => {
+      if (ind == monthNum) {
+        monthStr = month;
       }
-    }
+    });
 
-    spanDayOfTheWeek.textContent = dayOfTheWeekStr;
-    spanDay.textContent = day;
-    spanMonth.textContent = monthStr;
-    spanYear.textContent = year;
+    spanDayOfTheWeek.innerHTML = dayOfTheWeekStr;
+    spanDay.innerHTML = day;
+    spanMonth.innerHTML = monthStr;
+    spanYear.innerHTML = year;
   }
 }
 
