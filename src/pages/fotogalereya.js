ff771d298f924c82be4270c7d559beefe127637a
hopes91 <nadya.me91@gmail.com>
2019-06-07 15:01:32 +0300

    WIP: control the slider with arrows left/right

--- src/pages/fotogalereya.js
+++ src/pages/fotogalereya.js
@@ -4,6 +4,19 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import OrnamentMain from '../components/ornamentMain'
 
+import bibliosumerki from '../images/photogallery/bibliosumerki.jpg'
+import bibliosumerki2 from '../images/photogallery/bibliosumerki-2.jpg'
+import bibliosumerki3 from '../images/photogallery/bibliosumerki-3.jpg'
+import shlyapnayaVecherinka from '../images/photogallery/shlyapnaya-vecherinka.jpg'
+import shlyapnayaVecherinka2 from '../images/photogallery/shlyapnaya-vecherinka-2.jpg'
+import shlyapnayaVecherinka3 from '../images/photogallery/shlyapnaya-vecherinka-3.jpg'
+import shlyapnayaVecherinka4 from '../images/photogallery/shlyapnaya-vecherinka-4.jpg'
+import shlyapnayaVecherinka5 from '../images/photogallery/shlyapnaya-vecherinka-5.jpg'
+import chasPamyatiNaKruzhkeBiblionik from '../images/photogallery/chas-pamyati-na-kruzhke-biblionik.jpg'
+import bibliopiknik1Maya from '../images/photogallery/bibliopiknik-1-maya.jpg'
+import bibliochuchelo from '../images/photogallery/bibliochuchelo-dlya-biblionochi.jpg'
+import detsadovtsyVMaskah from '../images/photogallery/detsadovtsy-v-maskah-na-zanyatii-kruzhka-biblionik.jpg'
+import vystavkaTeatrIBiblioteka from '../images/photogallery/vystavka-teatr-i-biblioteka.jpg'
 import viktorinaNaDomuDlyaDevochkiSOV from '../images/photogallery/viktorina-na-domu-dlya-devochki-s-organichennymi-vozmozhnostyami.jpg'
 import chasZozhVperedPoDorogeZdoroviya from '../images/photogallery/chas-zozh-vpered-po-doroge-zdoroviya.jpg'
 import semejnyjKlub29Marta from '../images/photogallery/semejnyj-klub-29-marta.jpg'
@@ -26,6 +39,19 @@ import novogodnijVecherNaSemejnomKlube2 from '../images/photogallery/novogodnij-
 import novogodnijVecherNaSemejnomKlube3 from '../images/photogallery/novogodnij-vecher-na-semejnom-klube-3.jpg'
 
 const photos2019 = [
+  { src: bibliosumerki, alt: 'Библиосумерки' },
+  { src: bibliosumerki2, alt: 'Библиосумерки 2' },
+  { src: bibliosumerki3, alt: 'Библиосумерки 3' },
+  { src: shlyapnayaVecherinka, alt: 'Шляпная вечеринка' },
+  { src: shlyapnayaVecherinka2, alt: 'Шляпная вечеринка 2' },
+  { src: shlyapnayaVecherinka3, alt: 'Шляпная вечеринка 3' },
+  { src: shlyapnayaVecherinka4, alt: 'Шляпная вечеринка 4' },
+  { src: shlyapnayaVecherinka5, alt: 'Шляпная вечеринка 5' },
+  { src: chasPamyatiNaKruzhkeBiblionik, alt: 'Час памяти на кружке «Библионик»' },
+  { src: bibliopiknik1Maya, alt: 'Библиопикник 1 мая' },
+  { src: bibliochuchelo, alt: 'Библиочучело для Библионочи' },
+  { src: detsadovtsyVMaskah, alt: 'Детсадовцы в масках на кружке «Библионик»' },
+  { src: vystavkaTeatrIBiblioteka, alt: 'Выставка «Театр и библиотека»' },
   { src: viktorinaNaDomuDlyaDevochkiSOV, alt: 'Викторина на дому для девочки с ограниченными возможностями' },
   { src: chasZozhVperedPoDorogeZdoroviya, alt: 'Час ЗОЖ вперёд по дороге здоровья' },
   { src: semejnyjKlub29Marta, alt: 'Семейный клуб 29 марта' },
@@ -50,7 +76,7 @@ const photos2018 = [
   { src: novogodnijVecherNaSemejnomKlube3, alt: 'Новогодний вечер на семейном клубе 3' }
 ]
 
-const allPhotos = photos2019.concat(photos2018)
+let allPhotos = photos2019.concat(photos2018)
 let mainSlide = null
 let start = null
 
@@ -58,23 +84,31 @@ class PhotogalleryPage extends Component {
   constructor(props) {
     super(props)
 
+    // this.showFirstSlideOnKeyPress = this.showFirstSlideOnKeyPress.bind(this)
     this.showFirstSlide = this.showFirstSlide.bind(this)
     this.showSlides = this.showSlides.bind(this)
+    // this.handleArrowsOnKeyPress = this.handleArrowsOnKeyPress.bind(this)
     this.handleArrows = this.handleArrows.bind(this)
     this.currentSlide = this.currentSlide.bind(this)
+    // this.hideSliderOnKeyPress = this.hideSliderOnKeyPress.bind(this)
     this.hideSlider = this.hideSlider.bind(this)
   }
 
   componentDidMount() {
-    for (let i = 0; i < allPhotos.length; i++) {
-      const dot = document.createElement('span')
-      dot.setAttribute('class', 'dot')
-      document.querySelector('.dots').appendChild(dot)
-    }
+    window.addEventListener('keypress', this.handleArrowsOnKeyPress)
+	  window.addEventListener('keypress', this.hideSliderOnKeyPress)
+  }
+
+  showFirstSlideOnKeyPress(event) {
+  	if (event.key !== 'Enter') return;
+
+    this.showFirstSlide(event)
   }
 
   showFirstSlide(event) {
     const slides = document.querySelectorAll('.slide')
+
+    this.createDots()
 
     document.getElementById('back').style.display = 'block'
 
@@ -92,6 +126,15 @@ class PhotogalleryPage extends Component {
         this.handleArrows()
       }
     })
+  }
+
+  handleArrowsOnKeyPress(event) {
+    console.log(event.key)
+  	if (event.key === 'ArrowLeft') {
+  		this.handleArrows(-1)
+  	} else if (event.key === 'ArrowRight') {
+  		this.handleArrows(1)
+  	}
   }
 
   handleArrows(index) {
@@ -144,6 +187,20 @@ class PhotogalleryPage extends Component {
     }
   }
 
+  createDots() {
+    for (let i = 0; i < allPhotos.length; i++) {
+      const dot = document.createElement('span')
+      dot.setAttribute('class', 'dot')
+      document.querySelector('.dots').appendChild(dot)
+    }
+  }
+
+  hideSliderOnKeyPress(event) {
+  	if (event.key === 'Escape') return;
+
+    this.hideSlider();
+  }
+
   hideSlider() {
     mainSlide = null
     start = null
@@ -166,6 +223,7 @@ class PhotogalleryPage extends Component {
                 <img
                   src={src} alt={alt} title='Нажмите, чтобы посмотреть в большом разрешении' key={src}
                   className='mini-photo'
+                  onKeyPress={this.showFirstSlideOnKeyPress}
                   onClick={this.showFirstSlide}
                 />
               )
@@ -181,6 +239,7 @@ class PhotogalleryPage extends Component {
                 <img
                   src={src} alt={alt} title='Нажмите, чтобы посмотреть в большом разрешении' key={src}
                   className='mini-photo'
+                  onKeyPress={this.showFirstSlideOnKeyPress}
                   onClick={this.showFirstSlide}
                 />
               )
