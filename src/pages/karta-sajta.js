0257a1c061607c4dbbac65153f4c19d0e9cc6804
hopes91 <nadya.me91@gmail.com>
2019-06-10 09:26:23 +0300

    update SiteMapPage

--- src/pages/karta-sajta.js
+++ src/pages/karta-sajta.js
@@ -12,7 +12,7 @@ const linksSectionTwo = [
   { to: '/rezhim-raboty-biblioteki', page: 'rezhim-raboty-biblioteki', title: 'Режим работы библиотеки' }
 ]
 const linksSectionThree = [
-  { to: '/afisha', page: 'afisha', title: 'Афиша мая 2019 года' },
+  { to: '/afisha', page: 'afisha', title: 'Афиша лета 2019 года' },
   { to: '/afisha-semejnyj-klub-ot-vsej-dushi', page: 'afisha-semejnyj-klub-ot-vsej-dushi', title: 'Программа семейного клуба «От всей души»' },
   { to: '/afisha-kruzhok-biblionik', page: 'afisha-kruzhok-biblionik', title: 'Программа кружка «Библионик»' },
   { to: '/proshedshie-sobytiya-goda', page: 'proshedshie-sobytiya-goda', title: 'Прошедшие события 2019 года' }
