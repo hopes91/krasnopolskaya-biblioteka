88e179f7ed7fa2c547294341366dcd177cf80894
hopes91 <nadya.me91@gmail.com>
2019-06-11 14:40:12 +0300

    - => __

--- src/pages/rezhim-raboty-biblioteki.js
+++ src/pages/rezhim-raboty-biblioteki.js
@@ -12,14 +12,14 @@ const WorkSchedulePage = () => (
     <div className='page page_about page_about_schedule'>
       <OrnamentMain />
       <h2 className='page__title'>Режим работы библиотеки</h2>
-      <p className='page_about_schedule-days'><strong>Понедельник — четверг</strong></p>
+      <p className='page_about_schedule__days'><strong>Понедельник — четверг</strong></p>
       <p className='page_about_schedule_time'>13:00 — 16:30</p>
-      <p className='page_about_schedule-days'><strong>Пятница</strong></p>
+      <p className='page_about_schedule__days'><strong>Пятница</strong></p>
       <p className='page_about_schedule_time'>13:00 — 16:00</p>
-      <p className='page_about_schedule-days'><strong>Суббота, воскресенье</strong></p>
+      <p className='page_about_schedule__days'><strong>Суббота, воскресенье</strong></p>
       <p className='page_about_schedule_time'>выходные дни</p>
-      <p className='page_about_schedule-days'><strong>Раз в месяц занятие семейного клуба «От всей души»</strong></p>
-      <p className='page_about_schedule-time'>19:00 — 21:00</p>
+      <p className='page_about_schedule__days'><strong>Раз в месяц занятие семейного клуба «От всей души»</strong></p>
+      <p className='page_about_schedule__time'>19:00 — 21:00</p>
     </div>
   </Layout>
 )
