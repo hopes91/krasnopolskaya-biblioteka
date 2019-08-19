b485049f8635ad1cefbfaec97b42f1a879bcab5e
hopes91 <nadya.me91@gmail.com>
2019-08-19 11:32:08 +0300

    DecorTop: let => const, if refactor

--- src/components/decorTop.js
+++ src/components/decorTop.js
@@ -15,26 +15,24 @@ class DecorTop extends Component {
   }
 
   componentDidMount() {
-    let today = new Date()
-    let dayOfTheWeekNum = today.getDay()
-    let day = today.getDate()
-    let monthNum = today.getMonth()
-    let year = today.getFullYear()
+    const today = new Date()
+    const dayOfTheWeekNum = today.getDay()
+    const day = today.getDate()
+    const monthNum = today.getMonth()
+    const year = today.getFullYear()
 
     const daysOfTheWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
 
     daysOfTheWeek.forEach((day, ind) => {
-      if (ind !== dayOfTheWeekNum) return;
-
-      this.setState({ dayOfTheWeek: day })
+      ind === dayOfTheWeekNum &&
+        this.setState({ dayOfTheWeek: day })
     })
 
     const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
 
     months.forEach((month, ind) => {
-      if (ind !== monthNum) return;
-
-      this.setState({ month })
+      ind === monthNum &&
+        this.setState({ month })
     })
 
     this.setState({
