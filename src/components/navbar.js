241bc5f3841180d5983dece1f35009f9ed6ead09
hopes91 <nadya.me91@gmail.com>
2019-08-19 08:46:07 +0300

    move menu lists into separate file

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,22 +3,7 @@ import { Link } from 'gatsby'
 
 import '../styles/header/navbar.scss'
 
-const linksSectionTwo = [
-  { to: '/istoriya-biblioteki', page: 'istoriya-biblioteki', title: 'История библиотеки' },
-  { to: '/fotogalereya', page: 'fotogalereya', title: 'Фотогалерея' },
-  { to: '/o-bibliotekare', page: 'o-bibliotekare', title: 'О библиотекаре' },
-  { to: '/rezhim-raboty', page: 'rezhim-raboty', title: 'Режим работы' }
-]
-const linksSectionThree = [
-  { to: '/afisha', page: 'afisha', title: 'Афиша лета 2019 года' },
-  { to: '/ot-vsej-dushi', page: 'ot-vsej-dushi', title: 'Семейный клуб «От всей души»' },
-  { to: '/biblionik', page: 'biblionik', title: 'Кружок «Библионик»' },
-  { to: '/proshedshie-sobytiya', page: 'proshedshie-sobytiya', title: 'Прошедшие события 2019 года' }
-]
-const linksSectionFour = [
-  { to: '/poleznye-ssylki', page: 'poleznye-ssylki', title: 'Полезные ссылки' },
-  { to: '/bezopasnost-v-internete', page: 'bezopasnost-v-internete', title: 'Безопасность в Интернете' }
-]
+import { linksSectionTwo, linksSectionThree, linksSectionFour } from '../lists/menu'
 
 class Navbar extends Component {
   constructor(props) {
