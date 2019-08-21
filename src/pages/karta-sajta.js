7829546637addadfed91193393df3d18bedf3b9c
hopes91 <nadya.me91@gmail.com>
2019-08-21 11:37:17 +0300

    import menu list

--- src/pages/karta-sajta.js
+++ src/pages/karta-sajta.js
@@ -8,22 +8,7 @@ import DecorMain from '../components/decorMain'
 import '../styles/page/page.scss'
 import '../styles/page/_site-map.scss'
 
-const linksSectionTwo = [
-  { to: '/istoriya-biblioteki', page: 'istoriya-biblioteki', title: 'История библиотеки' },
-  { to: '/fotogalereya', page: 'fotogalereya', title: 'Фотогалерея' },
-  { to: '/o-bibliotekare', page: 'o-bibliotekare', title: 'О библиотекаре' },
-  { to: '/rezhim-raboty', page: 'rezhim-raboty', title: 'Режим работы' }
-]
-const linksSectionThree = [
-  { to: '/afisha', page: 'afisha', title: 'Афиша лета 2019 года' },
-  { to: '/ot-vsej-dushi', page: 'ot-vsej-dushi', title: 'Семейный клуб «От всей души»' },
-  { to: '/biblionik', page: 'biblionik', title: 'Кружок «Библионик»' },
-  { to: '/proshedshie-sobytiya', page: 'proshedshie-sobytiya', title: 'Прошедшие события 2019 года' }
-]
-const linksSectionFour = [
-  { to: '/poleznye-ssylki', page: 'poleznye-ssylki', title: 'Полезные ссылки' },
-  { to: '/bezopasnost-v-internete', page: 'bezopasnost-v-internete', title: 'Безопасность в Интернете' }
-]
+import { linksSectionTwo, linksSectionThree, linksSectionFour } from '../lists/menu'
 
 const SiteMapPage = () => (
   <Layout>
