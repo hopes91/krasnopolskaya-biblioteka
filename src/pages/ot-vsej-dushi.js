75c3b3ff6dbaed21e61c447aa47a118fae32efdb
hopes91 <nadya.me91@gmail.com>
2019-08-21 12:48:25 +0300

    import ot vsej dushi events list

--- src/pages/ot-vsej-dushi.js
+++ src/pages/ot-vsej-dushi.js
@@ -7,17 +7,7 @@ import DecorMain from '../components/decorMain'
 import '../styles/page/page.scss'
 import '../styles/page/_events.scss'
 
-const otVsejDushiEvents = [
-  { posterDate: 'Январь', posterEvents: 'Святочный вечер' },
-  { posterDate: 'Февраль', posterEvents: 'Вечер к Дню влюблённых' },
-  { posterDate: 'Март', posterEvents: 'Вечер юмора' },
-  { posterDate: 'Апрель', posterEvents: 'Огородные хлопоты' },
-  { posterDate: 'Май', posterEvents: 'Вечер отдыха к Международному дню семьи' },
-  { posterDate: 'Август', posterEvents: 'Собрал нас вместе Спас' },
-  { posterDate: 'Октябрь', posterEvents: 'Осенние дары' },
-  { posterDate: 'Ноябрь', posterEvents: 'Вечер по толерантности (к Году Турции в России)' },
-  { posterDate: 'Декабрь', posterEvents: 'Новогодняя сказка' }
-]
+import { otVsejDushiEvents } from '../lists/otVsejDushiEvents'
 
 const PosterOtVsejDushiPage = () => (
   <Layout>
