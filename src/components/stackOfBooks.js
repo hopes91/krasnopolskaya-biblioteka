2b23574b2fb52c3461d9bb845c1eda4b843099a0
hopes91 <nadya.me91@gmail.com>
2019-08-26 08:04:51 +0300

    .stack-of-books => .decor_stack-of-books

--- src/components/stackOfBooks.js
+++ src/components/stackOfBooks.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import '../styles/decor/stack-of-books.scss'
+import '../styles/decor/_stack-of-books.scss'
 
 import stackOfBooks from '../images/decor/stack-of-books-graphic-illustration.png'
 
 const StackOfBooks = () => (
-  <div className='stack-of-books'>
+  <div className='decor_stack-of-books'>
     <a href='https://www.freepik.com/free-vector/stack-of-books-graphic-illustration_2632998.htm' target='_blank' rel='noopener noreferrer' tabIndex='-1'>
       <img src={stackOfBooks} alt='Стопка книг' />
     </a>
