483aa6ec6e353aa2055053d80ddd40710c9f6d63
hopes91 <nadya.me91@gmail.com>
2019-08-26 08:23:26 +0300

    change gatsby-plugin-manifest options a bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Nadya F-a`,
+        short_name: `hopes91`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
