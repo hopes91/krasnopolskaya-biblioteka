697b4a01dbe9e482d6f5e3587aa59b7827707528
hopes91 <nadya.me91@gmail.com>
2019-09-04 13:26:15 +0300

    change classes of the site's map page

--- src/components/siteMapSection.js
+++ src/components/siteMapSection.js
@@ -4,11 +4,11 @@ import { Link } from 'gatsby'
 const siteMapSection = ({ sectionTitle, links }) => (
   <div className='links-section'>
     <p className='links-section__title'>{sectionTitle}</p>
-    <nav className='links-section__list'>
+    <nav className='links-section__links'>
       {links.map(link => {
         const { to, page, title } = link
 
-        return <Link to={to} key={page} className='links-section__list-item'>
+        return <Link to={to} key={page} className='links-section__links-item'>
           {title}
         </Link>
       })}
