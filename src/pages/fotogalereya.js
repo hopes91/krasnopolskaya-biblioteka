e6dfa5b0c98dbd9870af55c46182631318440fa9
hopes91 <nadya.me91@gmail.com>
2019-10-15 14:58:19 +0300

    refactor photogallery

--- src/pages/fotogalereya.js
+++ src/pages/fotogalereya.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Photogallery from '../components/photogallery'
@@ -9,161 +9,142 @@ const allPhotos = photos2019.concat(photos2018)
 let mainSlide = null
 let start = null
 
-class PhotogalleryPage extends Component {
-  constructor(props) {
-    super(props)
+const showFirstSlideOnKeyDown = event => {
+  event.key === 'Enter' &&
+    showFirstSlide(event)
+}
 
-    this.showFirstSlideOnKeyDown = this.showFirstSlideOnKeyDown.bind(this)
-    this.showFirstSlide = this.showFirstSlide.bind(this)
-    this.handleArrowsOnKeyDown = this.handleArrowsOnKeyDown.bind(this)
-    this.handleArrows = this.handleArrows.bind(this)
-    this.hideSliderOnKeyDown = this.hideSliderOnKeyDown.bind(this)
-    this.hideSlider = this.hideSlider.bind(this)
-  }
+const showFirstSlide = event => {
+  const back = document.querySelector('.page_about_photogallery-back')
+  const slides = document.querySelectorAll('.page_about_photogallery-back__slide')
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleArrowsOnKeyDown)
-	  document.addEventListener('keydown', this.hideSliderOnKeyDown)
-  }
+  if (back.className.match('opened')) return;
 
-  showFirstSlideOnKeyDown(event) {
-  	event.key === 'Enter' &&
-      this.showFirstSlide(event)
-  }
+  createDots()
 
-  showFirstSlide(event) {
-    const back = document.querySelector('.page_about_photogallery-back')
-    const slides = document.querySelectorAll('.page_about_photogallery-back__slide')
+  back.classList.remove('closed')
+  back.classList.add('opened')
 
-    if (back.className.match('opened')) return;
+  const clickedAlt = event.target.getAttribute('alt')
+  let altToCompare = null
 
-    this.createDots()
+  slides.forEach((slide, index) => {
+    altToCompare = slide.getAttribute('alt')
+    slide.style.display = 'none'
 
-    back.classList.remove('closed')
-    back.classList.add('opened')
-
-    const clickedAlt = event.target.getAttribute('alt')
-    let altToCompare = null
-
-    slides.forEach((slide, index) => {
-      altToCompare = slide.getAttribute('alt')
-      slide.style.display = 'none'
-
-      if (clickedAlt === altToCompare) {
-        slide.style.display = 'block'
-        mainSlide = index
-        start = index
-        this.handleArrows()
-      }
-    })
-  }
-
-  handleArrowsOnKeyDown(event) {
-    const back = document.querySelector('.page_about_photogallery-back')
-
-    if (back.className.match('closed')) return;
-
-  	if (event.key === 'ArrowLeft') {
-  		this.handleArrows(-1)
-  	} else if (event.key === 'ArrowRight') {
-  		this.handleArrows(1)
-  	}
-  }
-
-  handleArrows(index) {
-    const slides = document.querySelectorAll('.page_about_photogallery-back__slide')
-    const prevSlide = document.querySelector('.page_about_photogallery-back__prev-slide')
-    const nextSlide = document.querySelector('.page_about_photogallery-back__next-slide')
-
-    if ((index === -1 && mainSlide === 0) ||
-			(index === 1 && mainSlide === slides.length - 1)) {
-  		index = null
-  	} else if (index === undefined) {
-  		mainSlide = start
-  	} else {
-  		mainSlide += index
-  	}
-
-  	if (mainSlide === 0) {
-  		prevSlide.style.display = 'none'
-  	} else {
-  		prevSlide.style.display = 'block'
-  	}
-
-  	if (mainSlide === slides.length - 1) {
-  		nextSlide.style.display = 'none'
-  	} else {
-  		nextSlide.style.display = 'block'
-  	}
-
-  	this.showSlides()
-  }
-
-  showSlides() {
-    const slides = document.querySelectorAll('.page_about_photogallery-back__slide')
-
-    slides.forEach(slide => slide.style.display = 'none')
-
-    for (let i = start; i < slides.length; i++) {
-      slides[mainSlide].style.display = 'block'
-      this.currentSlide()
+    if (clickedAlt === altToCompare) {
+      slide.style.display = 'block'
+      mainSlide = index
+      start = index
+      handleArrows()
     }
+  })
+}
+
+const handleArrowsOnKeyDown = event => {
+  const back = document.querySelector('.page_about_photogallery-back')
+
+  if (back.className.match('closed')) return;
+
+  event.key === 'ArrowLeft' &&
+    handleArrows(-1)
+
+  event.key === 'ArrowRight' &&
+    handleArrows(1)
+}
+
+const handleArrows = index => {
+  const slides = document.querySelectorAll('.page_about_photogallery-back__slide')
+  const prevSlide = document.querySelector('.page_about_photogallery-back__prev-slide')
+  const nextSlide = document.querySelector('.page_about_photogallery-back__next-slide')
+
+  (index === -1 && mainSlide === 0) || (index === 1 && mainSlide === slides.length - 1) ?
+    index = null :
+  index === undefined ?
+    mainSlide = start :
+    mainSlide += index
+
+  mainSlide === 0 ?
+    prevSlide.style.display = 'none' :
+    prevSlide.style.display = 'block'
+
+  mainSlide === slides.length - 1 ?
+    nextSlide.style.display = 'none' :
+    nextSlide.style.display = 'block'
+
+  showSlides()
+}
+
+const showSlides = () => {
+  const slides = document.querySelectorAll('.page_about_photogallery-back__slide')
+
+  slides.forEach(slide => slide.style.display = 'none')
+
+  for (let i = start; i < slides.length; i++) {
+    slides[mainSlide].style.display = 'block'
+
+    currentSlide()
   }
+}
 
-  currentSlide() {
-    const dots = document.querySelectorAll('.page_about_photogallery-back__dot')
+const currentSlide = () => {
+  const dots = document.querySelectorAll('.page_about_photogallery-back__dot')
 
-    dots.forEach(dot => dot.style.backgroundColor = 'oldlace')
+  dots.forEach(dot => dot.style.backgroundColor = 'oldlace')
 
-    for (let i = start; i < dots.length; i++) {
-      dots[mainSlide].style.backgroundColor = 'saddlebrown'
-    }
+  for (let i = start; i < dots.length; i++) {
+    dots[mainSlide].style.backgroundColor = 'saddlebrown'
   }
+}
 
-  createDots() {
-    const dots = document.querySelector('.page_about_photogallery-back__dots')
+const createDots = () => {
+  const dots = document.querySelector('.page_about_photogallery-back__dots')
 
-    dots.innerHTML = ''
+  dots.innerHTML = ''
 
-    allPhotos.forEach(photo => {
-      const dot = document.createElement('span')
-      dot.setAttribute('class', 'page_about_photogallery-back__dot')
-      dots.appendChild(dot)
-    })
-  }
+  allPhotos.forEach(photo => {
+    const dot = document.createElement('span')
+    dot.setAttribute('class', 'page_about_photogallery-back__dot')
+    dots.appendChild(dot)
+  })
+}
 
-  hideSliderOnKeyDown(event) {
-  	event.key === 'Escape' &&
-      this.hideSlider()
-  }
+const hideSliderOnKeyDown = event => {
+  event.key === 'Escape' &&
+    hideSlider()
+}
 
-  hideSlider() {
-    const back = document.querySelector('.page_about_photogallery-back')
+const hideSlider = () => {
+  const back = document.querySelector('.page_about_photogallery-back')
 
-    if (back.className.match('closed')) return;
+  if (back.className.match('closed')) return;
 
-    back.classList.remove('opened')
-    back.classList.add('closed')
-    mainSlide = null
-    start = null
-  }
+  back.classList.remove('opened')
+  back.classList.add('closed')
+  mainSlide = null
+  start = null
+}
 
-  render() {
-    return (
-      <Layout>
-        <SEO title='Фотогалерея' keywords={[`фотографии краснопольской библиотеки`, `фотогалерея`, `краснопольская библиотека`, `краснополье`, `сунский район`, `суна`, `кировская область`]} />
-        <Photogallery
-          photos2019={photos2019}
-          photos2018={photos2018}
-          allPhotos={allPhotos}
-          showFirstSlideOnKeyDown={this.showFirstSlideOnKeyDown}
-          showFirstSlide={this.showFirstSlide}
-          handleArrows={this.handleArrows}
-          hideSlider={this.hideSlider}
-        />
-      </Layout>
-    )
-  }
+const PhotogalleryPage = () => {
+  useEffect(() => {
+    document.addEventListener('keydown', handleArrowsOnKeyDown)
+	  document.addEventListener('keydown', hideSliderOnKeyDown)
+  })
+
+  return (
+    <Layout>
+      <SEO title='Фотогалерея' keywords={[`фотографии краснопольской библиотеки`, `фотогалерея`, `краснопольская библиотека`, `краснополье`, `сунский район`, `суна`, `кировская область`]} />
+      <Photogallery
+        photos2019={photos2019}
+        photos2018={photos2018}
+        allPhotos={allPhotos}
+        showFirstSlideOnKeyDown={showFirstSlideOnKeyDown}
+        showFirstSlide={showFirstSlide}
+        handleArrows={handleArrows}
+        hideSlider={hideSlider}
+      />
+    </Layout>
+  )
 }
 
 export default PhotogalleryPage
