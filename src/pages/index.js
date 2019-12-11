83ec319226b7c6ec1dd1abedf7b4e20affcbe14c
hopes91 <nadya.me91@gmail.com>
2019-12-11 18:45:00 +0300

    remove news: 2.12

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import DecorMain from '../components/decorMain'
 import '../styles/page/page.scss'
 import '../styles/page/news.scss'
 
-import news30122019 from '../images/news/30122019.jpg'
-import news09122019 from '../images/news/09122019.png'
 import news121020192 from '../images/news/12102019-2.jpg'
 import news08072019 from '../images/news/08072019.jpg'
 import news07062019 from '../images/news/07062019.jpg'
@@ -21,11 +19,6 @@ const IndexPage = () => (
     <div className='page news'>
       <DecorMain />
       <h2 className='page-title'>Новости</h2>
-      <section>
-        <h6>понедельник, 2 декабря 2019 год</h6>
-        <img src={news09122019} alt='9 декабря Лихановские чтения. Уроки нравственности "Понять и простить" по повести "Чистые камушки" и "Храните мир на Земле" по повести "Последние холода".' className='page-image' />
-        <img src={news30122019} alt='30 декабря мастер-класс на дому "Сделаем из бумаги мышку на новогоднюю ёлку" и новогодняя викторина.' className='page-image' />
-      </section>
       <section>
         <h6>суббота, 12 октября 2019 год</h6>
         <img src={news121020192} alt='За 9 месяцев 2019 года лучшие читатели-дети: Бобылев Руслан, Райлинг Женя, Вотинова Виолетта, Зворыгина Марьяна, Вотинова Ксения, Суходоева Таня. Самые активные посетители библиотечных мероприятий: Райлинг Женя, Ситников Кирилл, Перескоков Никита, Чекалкин Саша, Толмачёв Дима, Ральникова Полина.' className='page-image' />
