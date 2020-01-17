8cbc4ec7b0d788b82fe55787dd49a805eec70291
hopes91 <nadya.me91@gmail.com>
2020-01-17 17:48:00 +0300

    remove the new year's decor

--- src/components/decorTop.js
+++ src/components/decorTop.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../styles/decor/top.scss'
 
-import newYearDecorTop from '../images/decor/new-year-frame-2.png'
+// import newYearDecorTop from '../images/decor/new-year-frame-2.png'
 
 const DecorTop = ({ dayOfTheWeek, day, month, year }) => (
   <div className='decor-top'>
@@ -11,11 +11,11 @@ const DecorTop = ({ dayOfTheWeek, day, month, year }) => (
       <span className='date-month'>{month}</span>
       <span className='date-year'>{year}</span>
     </p>
-    <img
+    {/* <img
       src={newYearDecorTop}
       alt=''
       className='pic'
-    />
+    /> */}
   </div>
 )
 
