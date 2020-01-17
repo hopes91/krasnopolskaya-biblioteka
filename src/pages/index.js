a56386b4146ce33defcf6fa31a22951778a994eb
hopes91 <nadya.me91@gmail.com>
2020-01-17 17:36:51 +0300

    add news: 01.17

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import DecorMain from '../components/decorMain'
 import '../styles/page/page.scss'
 import '../styles/page/news.scss'
 
-
+import news17012020 from '../images/news/17012020.jpg'
 
 const IndexPage = () => (
   <Layout>
@@ -13,7 +13,10 @@ const IndexPage = () => (
     <div className='page news'>
       <DecorMain />
       <h2 className='page-title'>Новости</h2>
-
+      <section>
+        <h3>пятница, 17 января 2020 год</h3>
+        <img src={news17012020} alt='Список лучших читателей 2019 года' className='page-image' />
+      </section>
     </div>
   </Layout>
 )
