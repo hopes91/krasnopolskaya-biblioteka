93c2a66778a3c40d11c6d85cfb9effecff5e0d3d
hopes91 <nadya.me91@gmail.com>
2020-01-17 17:34:47 +0300

    remove the 2019 news

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,7 @@ import DecorMain from '../components/decorMain'
 import '../styles/page/page.scss'
 import '../styles/page/news.scss'
 
-import news13122019 from '../images/news/13122019.png'
-import news12122019 from '../images/news/12122019.jpg'
-import news08072019 from '../images/news/08072019.jpg'
-import news07062019 from '../images/news/07062019.jpg'
-import news05062019 from '../images/news/05062019.jpg'
-import news03062019 from '../images/news/03062019.jpg'
-import news20052019 from '../images/news/20052019.jpg'
-import news24032019 from '../images/news/24032019-2.jpg'
+
 
 const IndexPage = () => (
   <Layout>
@@ -20,14 +13,7 @@ const IndexPage = () => (
     <div className='page news'>
       <DecorMain />
       <h2 className='page-title'>Новости</h2>
-      <section>
-        <h3>понедельник, 3 июня 2019 год</h3>
-        <img src={news03062019} alt='Грамота Фалалеевой Е.Е. за 1-ое место в конкурсе проф. мастерства' className='page-image' />
-      </section>
-      <section>
-        <h3>понедельник, 20 мая 2019 год</h3>
-        <img src={news20052019} alt='Благодарственное письмо за активное участие в марафоне «Добрая Вятка»' className='page-image' />
-      </section>
+
     </div>
   </Layout>
 )
