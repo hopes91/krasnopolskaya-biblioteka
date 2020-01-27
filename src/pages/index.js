53b9da5b5640a756f79a96af5629b290b64242e2
hopes91 <nadya.me91@gmail.com>
2020-01-27 10:05:19 +0300

    add news: 01.27

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import DecorMain from '../components/decorMain'
 import '../styles/page/page.scss'
 import '../styles/page/news.scss'
 
+import news27012020 from '../images/news/27012020.jpg'
 import news17012020 from '../images/news/17012020.jpg'
 
 const IndexPage = () => (
@@ -13,6 +14,11 @@ const IndexPage = () => (
     <div className='page news'>
       <DecorMain />
       <h2 className='page-title'>Новости</h2>
+      <section>
+        <h3>понедельник, 27 января 2020 год</h3>
+        <p>Краснопольская библиотека участвует в сетевой акции «ПоЧитатели Чехова» к 160-летию со дня рождения писателя.</p>
+        <img src={news27012020} alt='Краснопольская библиотека участвует в сетевой акции «ПоЧитатели Чехова» к 160-летию со дня рождения писателя' className='page-image' />
+      </section>
       <section>
         <h3>пятница, 17 января 2020 год</h3>
         <img src={news17012020} alt='Список лучших читателей 2019 года' className='page-image' />
