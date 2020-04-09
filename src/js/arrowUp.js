bfa75af47c73d924515fd9cb2439137d97b9566e
hopes91 <nadya.me91@gmail.com>
2020-04-09 17:08:54 +0300

    add script and comment it out for toggling the arrow up

--- src/js/arrowUp.js
+++ src/js/arrowUp.js
@@ -1,3 +1,17 @@
+// const showHideArrowUp = () => {
+// 	const arrowUp = document.getElementById('arrow-up');
+//
+// 	if (window.pageYOffset > window.innerHeight / 3) {
+// 		arrowUp.style.display = 'inline-block';
+//     arrowUp.setAttribute('tabindex', '0');
+// 	}
+//
+// 	if (window.pageYOffset === 0) {
+// 		arrowUp.style.display = 'none';
+//     arrowUp.setAttribute('tabindex', '-1');
+// 	}
+// };
+
 function scrollToTop(event) {
   event.preventDefault();
 
@@ -34,4 +48,5 @@ function scrollToTop(event) {
   }
 }
 
+// window.addEventListener('scroll', showHideArrowUp);
 document.getElementById('arrow-up').addEventListener('click', scrollToTop);
