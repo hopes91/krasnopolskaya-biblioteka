7a7bb511b0ead4d91ac241f731531814ce4c9d58
hopes91 <nadya.me91@gmail.com>
2020-11-29 10:42:03 +0300

    remove dots from the photogallery

--- src/js/photogallery.js
+++ src/js/photogallery.js
@@ -30,8 +30,6 @@ const manageSliderOnKeyDown = event => {
 
 const showFirstSlide = event => {
   if (back.className.match('closed')) {
-    createDots();
-
     back.className = 'popup_back opened';
 
     const clickedAlt = event.target.getAttribute('alt');
@@ -82,31 +80,7 @@ const showSlides = () => {
 
   for (let i = start; i < slides.length; i++) {
     slides[mainSlide].style.display = 'block';
-
-    currentSlide();
   }
-};
-
-const currentSlide = () => {
-  const dots = document.querySelectorAll('.dot');
-
-  dots.forEach(dot => dot.style.backgroundColor = 'oldlace');
-
-  for (let i = start; i < dots.length; i++) {
-    dots[mainSlide].style.backgroundColor = 'saddlebrown';
-  }
-};
-
-const createDots = () => {
-  const dotsContainer = document.querySelector('.dots');
-
-  dotsContainer.innerHTML = '';
-
-  slides.forEach(slide => {
-    const dot = document.createElement('span');
-    dot.setAttribute('class', 'dot');
-    dotsContainer.appendChild(dot);
-  });
 };
 
 const hideSlider = () => {
